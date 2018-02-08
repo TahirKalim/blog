d4d3e0dd45583d68eb3a265a276dce5f277a1f3b
tahirkalim <thrkalim@gmail.com>
2018-02-08 01:35:16 +0100

    adding article model and links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205024508) do
+ActiveRecord::Schema.define(version: 20180207220739) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "title"
